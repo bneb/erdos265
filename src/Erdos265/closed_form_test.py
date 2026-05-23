9a7aeb9b8ca2af4b1f81ae51290edcc3cce00824
Kevin <oconn.kv@gmail.com>
2026-05-23 14:07:43 -0700

    restart

--- src/Erdos265/closed_form_test.py
+++
@@ -1,8 +0,0 @@
-import sys
-from fractions import Fraction
-
-sys.set_int_max_str_digits(1000000)
-
-# Kovač and Tao used interval halving. 
-# We need to find if there is a deterministic parity-switching logic.
-# Let's write a BFS that ONLY branches when absolutely necessary to avoid a dead end.
